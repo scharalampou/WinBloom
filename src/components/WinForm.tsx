8c2e37a69696bfaebad20ecf9817639386a3ae6d
Sofia Charalampous <scharalampous84@gmail.com>
2026-01-05 23:58:33 +0000

    Try fixing this error: `Runtime ReferenceError: useState is not defined.

--- src/components/WinForm.tsx
+++ src/components/WinForm.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
